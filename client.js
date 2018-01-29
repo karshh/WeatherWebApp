5267085d3536467b9fe82ed0da6a1f863b3d5eaa
karshh <karsharma24@gmail.com>
2018-01-28 22:18:30 -0700

    Completed data abstraction between client and server js.

--- client.js
+++ client.js
@@ -1,44 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Fills each table row with temperature data.
+//
 
-const server = require("./server.js");
+function writeTable(data) {
+	$.each(data, (i, val) => {
+		var date = new Date(val.time * 1000)
+		var hour = date.getHours();
+		hour = (hour == 0 ? 12 : hour % 12 + 1); // formatting hour to 12-hour clock.
+		var min = date.getMinutes();
+		min = (min < 10 ? "0" + min : min); 	// formatting minutes.
+		var meridiem = (hour < 12 ? "AM" : "PM");
 
-$(document).ready(function() {
-	const data = server.getWeatherJSON();
-	var hour0 = (new Date(data[0].time * 1000)).getHours();
-    var hour1 = (new Date(data[1].time * 1000)).getHours();
-    var hour2 = (new Date(data[2].time * 1000)).getHours();
-
-    var minute0 = (new Date(data[0].time * 1000)).getMinutes();
-    var minute1 = (new Date(data[1].time * 1000)).getMinutes();
-    var minute2 = (new Date(data[2].time * 1000)).getMinutes();
-
-    $('#weatherTable').append(`
+		$('#weatherTable').append(`
             <tr>
-                <td align=\"center\">` 
-                    + (hour0 == 0 ? 12 : hour0 % 12 + 1) + ":"  // HOUR in 12 hour format
-                    + (minute0 < 10 ? "0" + minute0 : minute0)  // minute
-                    + (hour0 < 12 ? "AM" : "PM") +              // AM/PM
-                `</td>
-                <td align=\"center\">` + data[0].temperature + `<sup>o</sup></td>
-                <td align=\"center\">` + data[0].humidity + `</td>
-            </tr>
-
-            <tr>
-                <td align=\"center\">` 
-                    + (hour1 == 0 ? 12 : hour1 % 12 + 1) + ":"  // HOUR in 12 hour format
-                    + (minute1 < 10 ? "0" + minute1 : minute1)  // minute
-                    + (hour1 < 12 ? "AM" : "PM") +              // AM/PM
-                `</td>
-                <td align=\"center\">` + data[1].temperature + `<sup>o</sup></td>
-                <td align=\"center\">` + data[1].humidity + `</td>
-            </tr>
-            <tr>
-                <td align=\"center\">` 
-                    + (hour2 == 0 ? 12 : hour2 % 12 + 1) + ":"  // HOUR in 12 hour format
-                    + (minute2 < 10 ? "0" + minute2 : minute2)  // minute
-                    + (hour2 < 12 ? "AM" : "PM") +              // AM/PM
-                `</td>
-                <td align=\"center\">` + data[2].temperature + `<sup>o</sup></td>
-                <td align=\"center\">` + data[2].humidity + `</td>
+                <td>` + hour + ":" + min + " " + meridiem + `</td>
+                <td>` + val.temperature + `<sup>o</sup></td>
+                <td>` + val.humidity + `</td>
             </tr>
     	`);
+	});
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Upon every page load, get weather data.
+//
+
+$(document).ready(function() {
+
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:8888/weatherData',
+		success: (dat) => writeTable(dat)
+	});
+	
 });
--- server.js
+++ server.js
@@ -1,43 +1,69 @@
 var express = require("express");
 var darkSkyAPI = require("dark-sky-api")
 var axios = require("axios");
-var app = express()
+var app = express();
 
-var PORT = 8888
+var PORT = 8888;
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Dark-Sky-API URL data.
+//
+
 var key = ""; // enter your key here.
 var param = "51.0659,-114.0914" // co-ordinates of SAIT.
 var units = "si"; // celcius
+
+var data = null; // null for now, whenever we make a GET call to Dark Sky API, 
+                 // we fill this variable with weather data.
+
 var darkSkyURL = "https://api.darksky.net/forecast/" + key + "/" + param + "?units=" + units;
 
-var index = "index.html";
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Load CSS/JS files onto server.
+//
 
-function getWeatherJSON() {
+app.use('/jquery.js', express.static(__dirname + "/" + '/jquery.js'));
+app.use('/client.js', express.static(__dirname + "/" + '/client.js'));
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  This code is executed when we access the home page of the host.
+//
+
+app.get('/', (request, response) => {
     axios({
         method: "GET",
         url: darkSkyURL
-    }).then(function(res){
-        return res.data.hourly.data;
+    }).then(function(res) {
+        data = res.data.hourly.data.slice(0,3);
+        response.sendFile(__dirname + "/" + "index.html");
     }).catch(function(err) {
-            console.log("ERROR:" + err);
-            return err;
-
+        console.log("ERROR:" + err);
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.end(err);
     });
-}
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Whenever we load the page, client.js will post request weather data once the 
+//  html doc has been fully loaded. The code here responds to that request, with the
+//  value we stored in data earlier.
+//  
+
+app.post('/weatherData', (req, res) => res.send(data));
 
 
-app.listen(PORT, function(){
-    var port = this.address().port;
-    console.log("App listening at http://localhost:%s\n", port);
-    });
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Begin listening on PORT.
+//
+
+var Server = app.listen(PORT, 
+    () => console.log("App listening at http://localhost:%s\n", Server.address().port));
 
 
-app.get('/', function(request, response) {
-    response.sendFile(__dirname + "/" + index)
-    });
-
-
-
-app.use('/jquery.js', express.static(__dirname + "/" + '/jquery.js'));
-
-app.use('/clientBrowserify.js', express.static(__dirname + "/" + '/clientBrowserify.js'));
-
+///////////////////////////////////////////////////////////////////////////////////////////////
